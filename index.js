6d99489191d44141506651f986d9ab6bcf50c86d
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-02-02 16:38:32 +0530

    add access

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 // end of middlewares
 
 // app.get("/test", (req, res) => {
