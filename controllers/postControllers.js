40fb070a5136ad6140a6c7d609d63ebe359dddf2
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-03-05 20:27:06 +0530

    edit blogs

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,35 +1,37 @@
 const fs = require("fs");
-
 const Post = require("../models/Post");
-
 const cloudinary = require("cloudinary").v2;
 
+// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+// Controller to post blogs
 exports.postBlogs = async (req, res) => {
-  console.log("POSTCONTROLLER --> Enter in postBlogs");
+  console.log("POSTCONTROLLER --> Entering postBlogs");
 
   try {
     if (!req.file) {
       return res.status(400).json({ error: "No file uploaded" });
     }
 
-    console.log("POSTCONTROLLER -->Img is there postBlogs");
+    console.log("POSTCONTROLLER --> Image is present in postBlogs");
 
     const imgFile = req.file;
     const { title, summary, content } = req.body;
 
+    // Upload image to Cloudinary
     const response = await cloudinary.uploader.upload(imgFile.path);
     const path = response.secure_url;
 
     console.log(
-      "POSTCONTROLLER -->Get the data and upload to multer succeceduklly"
+      "POSTCONTROLLER --> Data retrieved and uploaded to multer successfully"
     );
 
+    // Create post document
     const postDoc = await Post.create({
       title,
       summary,
@@ -37,8 +39,9 @@ exports.postBlogs = async (req, res) => {
       cover: path,
     });
 
-    console.log(" POSTCONTROLLER --> Post created succesfully");
+    console.log(" POSTCONTROLLER --> Post created successfully \n");
 
+    // Delete temporary image file
     fs.unlinkSync(imgFile.path);
 
     res.json(postDoc);
@@ -48,35 +51,38 @@ exports.postBlogs = async (req, res) => {
   }
 };
 
+// Controller to get all blogs
 exports.getBlogs = async (req, res) => {
-  console.log(" POSTCONTROLLER --> enter in get All blogs");
+  console.log(" POSTCONTROLLER --> Entering get All blogs");
 
   const posts = await Post.find();
 
-  console.log(" POSTCONTROLLER --> getted all blogs ");
+  console.log(" POSTCONTROLLER --> Retrieved all blogs \n");
 
   res.json(posts);
 };
 
+// Controller to get a single blog
 exports.getSingleBlogs = async (req, res) => {
-  console.log("POSTCONTROLLER --> Enter in single blogs");
+  console.log("POSTCONTROLLER --> Entering single blog");
   const { id } = req.params;
 
-  console.log("POSTCONTROLLER --> log the id=", id);
+  console.log("POSTCONTROLLER --> Logging the id=", id);
 
   const post = await Post.findById(id);
 
-  console.log("POSTCONTROLLER --> Get the single blogs");
+  console.log("POSTCONTROLLER --> Retrieved the single blog \n");
 
   res.json(post);
 };
 
+// Controller to delete a single blog
 exports.deleteSingleBlog = async (req, res) => {
-  console.log("POSTCONTROLLER --> Enter in delete blogs");
+  console.log("POSTCONTROLLER --> Entering delete blog");
 
   const postId = req.params.id;
 
-  console.log("POSTCONTROLLER --> log the postid=", postId);
+  console.log("POSTCONTROLLER --> Logging the post id=", postId);
 
   try {
     // Check if the post exists
@@ -89,7 +95,7 @@ exports.deleteSingleBlog = async (req, res) => {
     // Perform the deletion
     await post.deleteOne();
 
-    console.log("POSTCONTROLLER --> dleted succesfully");
+    console.log("POSTCONTROLLER --> Deleted successfully \n");
 
     res.status(200).json({ message: "Post deleted successfully" });
   } catch (error) {
